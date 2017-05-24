3d68aafe088c5078b5b07484c3ee35ed37b7b894
Alex Khatilov <alex@alex.cc>
2017-05-24 01:53:15 -0700

    Initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,15 @@
+node {
+def mvnHome
+stage('Git') {
+git 'https://github.com/khatilov/jenkins.git'
+mvnHome = tool 'Maven_3.5.0'
+}
+stage('Maven') {
+if (isUnix())  		{sh "'${mvnHome}/bin/mvn' clean compile findbugs:findbugs pmd:pmd pmd:cpd checkstyle:checkstyle"}
+else 		  		{bat(/"${mvnHome}\bin\mvn" clean compile pmd:pmd pmd:cpd findbugs:findbugs checkstyle:checkstyle/)}
+               }
+stage('PMD') 		{step([$class: 'PmdPublisher', pattern: '**/pmd*.xml'])}
+stage('CheckStyle')	{step([$class: 'CheckStylePublisher', pattern: '**/checkstyle*.xml'])}
+stage('FindBugs') 	{step([$class: 'FindBugsPublisher', pattern: '**/findbugs*.xml'])}
+stage('CPD') 		{step([$class: 'DryPublisher', pattern: '**/cpd.xml'])}
+}
---
+++ src/test/java/core/NewTest.java
@@ -0,0 +1,24 @@
+package core;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	@Test
+	public void test1() {
+		org.testng.Assert.assertTrue(Anagram.validate("Debit Card", "Bad Credit"), "Error message");
+		System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
+	} // method (test)
+
+	@Test
+	public void tes2t() {
+		org.testng.Assert.assertTrue(Anagram.validate("The Hurricanes", "These churn air"), "Error message");
+		System.out.println("The Hurricanes and These churn air is anagram? - "
+				+ Anagram.validate("The Hurricanes", "These churn air"));
+	} // method (test)
+
+	@Test
+	public void test3() {
+		org.testng.Assert.assertTrue(Anagram.validate("Охренеть", "Не хрена себе"), "Error message");
+		System.out.println("Охренеть and Не хрена себе is anagram? - " + Anagram.validate("Охренеть", "Не хрена себе"));
+	} // method (test)
+} // class
